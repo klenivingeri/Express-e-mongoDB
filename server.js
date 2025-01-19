e25c782f1d516354c61ff6f43c8faa3be0c02984
klenivingeri <libertti@live.com>
2025-01-19 11:12:47 -0300

    Adicionando nodemon e criando rotas

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ import http from 'http';
 
 const PORT = 3000
 
+const rotas = {
+    '/': 'Curso de Node.js',
+    '/livros': 'Entrei na rota Livros',
+    '/autores': 'Entrei na rota autores'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n');
+    console.log(req.headers)
+    res.end(rotas[req.url]);
 })
+
 
 server.listen(PORT, () => console.log('listening on port ' + PORT));
