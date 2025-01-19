257f893aad640619550e6c7bf82ab4d64c7a0a78
klenivingeri <libertti@live.com>
2025-01-19 20:50:09 -0300

    Criando a rota livros

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send('Curso no NODEJS')
-})
+const livros = [
+  {
+    id: 1,
+    titulo: "O Senhor dos Anéis",
+  },
+  {
+    id: 2,
+    titulo: "Pequeno Principe",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso no NODEJS");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
 
 export default app;
