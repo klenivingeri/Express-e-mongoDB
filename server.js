78e1b793a0c08c96dbb31259215c6ff009e2730e
klenivingeri <libertti@live.com>
2025-01-19 20:38:58 -0300

    Iniciando com Express

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
-import http from 'http';
+import app from './src/app.js'
 
-const PORT = 3000
+const PORT = 3000;
 
-const rotas = {
-    '/': 'Curso de Node.js',
-    '/livros': 'Entrei na rota Livros',
-    '/autores': 'Entrei na rota autores'
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    console.log(req.headers)
-    res.end(rotas[req.url]);
-})
-
-
-server.listen(PORT, () => console.log('listening on port ' + PORT));
+app.listen(PORT, () => console.log("listening on port " + PORT));
